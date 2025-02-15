22bcd825acb0c8cc36e90aeb945e8210d77068f4
Ricardo Zandonai <squall@outlook.com>
2025-02-15 12:52:45 -0300

    update rule to get value and update test

--- cmd/generate/config/rules/sonar.go
+++ cmd/generate/config/rules/sonar.go
@@ -10,19 +10,18 @@ func Sonar() *config.Rule {
 	r := config.Rule{
 		Description: "Uncovered a Sonar API token, potentially compromising software vulnerability scanning and code security.",
 		RuleID:      "sonar-api-token",
-		Regex:       utils.GenerateSemiGenericRegex([]string{"sonar[_.-]?(?:(?:login|token)[_.-]?)"}, utils.AlphaNumericExtended("40"), true),
+		Regex:       utils.GenerateSemiGenericRegex([]string{"sonar[_.-]?(login|token)"}, utils.AlphaNumericExtended("40"), true),
 		Keywords:    []string{"sonar"},
 	}
 
 	// validate
 	tps := utils.GenerateSampleSecrets("sonar", "12345678ABCDEFH1234567890ABCDEFH12345678")
 	tps = append(tps,
-		`const SONAR_LOGIN = "12345678ABCDEFH1234567890ABCDEFH12345678"`,      // gitleaks:allow
-		`SONAR_LOGIN := "12345678ABCDEFH1234567890ABCDEFH12345678"`,           // gitleaks:allow
-		`SONAR.LOGIN ::= "12345678ABCDEFH1234567890ABCDEFH12345678"`,          // gitleaks:allow
-		`SONAR.LOGIN :::= "12345678ABCDEFH1234567890ABCDEFH12345678"`,         // gitleaks:allow
-		`SONAR.LOGIN ?= "12345678ABCDEFH1234567890ABCDEFH12345678"`,           // gitleaks:allow
-		`name="sonar.login" value="12345678ABCDEFH1234567890ABCDEFH12345678"`, // gitleaks:allow
+		`const SONAR_LOGIN = "12345678ABCDEFH1234567890ABCDEFH12345678"`, // gitleaks:allow
+		`SONAR_LOGIN := "12345678ABCDEFH1234567890ABCDEFH12345678"`,      // gitleaks:allow
+		`SONAR.LOGIN ::= "12345678ABCDEFH1234567890ABCDEFH12345678"`,     // gitleaks:allow
+		`SONAR.LOGIN :::= "12345678ABCDEFH1234567890ABCDEFH12345678"`,    // gitleaks:allow
+		`SONAR.LOGIN ?= "12345678ABCDEFH1234567890ABCDEFH12345678"`,      // gitleaks:allow
 	)
 	return utils.Validate(r, tps, nil)
 }
